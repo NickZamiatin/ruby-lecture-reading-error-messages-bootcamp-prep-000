ea97aa75b98baabd423b85f6ddab3a017cac5bf2
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-10-04 19:05:32 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -3,7 +3,8 @@ describe "Not having any errors and being all green" do
   context 'NameError' do
     it 'raises a NameError when encountering undefined barewords' do
       expect{
-        "hello_world"
+        "hello world"
+        Hello world
         load './lib/a_name_error.rb'
       }.to_not raise_error
     end
